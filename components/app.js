2e492296ab947d07f5884d467d18ef3ddde1c861
Zack Kern <zkkern@gmail.com>
2020-09-22 19:34:52 -0700

    Added posting of new grades

--- components/app.js
+++ components/app.js
@@ -5,6 +5,9 @@ class App {
     this.gradeForm = gradeForm;
     this.handleGetGradesError = this.handleGetGradesError.bind(this);
     this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+    this.createGrade = this.createGrade.bind(this);
+    this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
+    this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
   }
   handleGetGradesError(error) {
     console.error(error);
@@ -34,6 +37,29 @@ class App {
     });
   }
   start() {
+    this.getGrades();
+    this.gradeForm.onSubmit(this.createGrade);
+  }
+  createGrade(name, course, grade) {
+    $.ajax({
+      method: "POST",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      data: {
+        name: name,
+        course: course,
+        grade: grade,
+      },
+      headers: {
+        "X-Access-Token": "obUmk8K2",
+      },
+      success: this.handleCreateGradeSuccess,
+      error: this.handleCreateGradeError,
+    });
+  }
+  handleCreateGradeError(error) {
+    console.error(error);
+  }
+  handleCreateGradeSuccess() {
     this.getGrades();
   }
 }
--- components/grade-form.js
+++ components/grade-form.js
@@ -2,6 +2,7 @@ class GradeForm {
   constructor(formElement) {
     this.formElement = formElement;
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.formElement.addEventListener("submit", this.handleSubmit);
   }
   onSubmit(createGrade) {
     this.createGrade = createGrade;
@@ -9,5 +10,11 @@ class GradeForm {
   handleSubmit(event) {
     event.preventDefault();
     console.log("handleSubmit called");
+    var myForm = new FormData(event.target);
+    var nameZ = myForm.get("name");
+    var courseZ = myForm.get("course");
+    var gradeZ = myForm.get("grade");
+    this.createGrade(nameZ, courseZ, gradeZ);
+    event.target.reset();
   }
 }
